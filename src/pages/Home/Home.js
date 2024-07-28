a030ec89050e959c0b6d708a883ca77c5e4b606a
VanPhuong03 <boss0352827813@gmail.com>
2024-07-28 12:06:14 +0700

    cập nhật api hiển thị thời tiết

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,137 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// // import axios from "axios";
-// import Container from "react-bootstrap/Container";
-// import Row from "react-bootstrap/Row";
-// import Col from "react-bootstrap/Col";
-// // import API_ENDPOINTS from "../../config/api";
-// import { fetchDashboardNews } from "../../services/newsService";
-// import RecommenNewsList from "../../components/RecommenNewsList/RecommenNewsList";
-// import LatestNewsList from "../../components/LatestNewsList/LatestNewsList";
-// import "./Home.scss";
-
-// function Home() {
-//   const [homedata, setNewsData] = useState([]);
-//   const [topViewedNews, setTopViewedNews] = useState([null]);
-//   const [loading, setLoading] = useState(false);
-
-//   useEffect(() => {
-//     const getNewsData = async () => {
-//       setLoading(true)
-//       try {
-//         const data = await fetchDashboardNews();
-//         const newData = await data.json();
-//         setNewsData(data);
-//         setLoading(false);
-//       } catch (error) {
-//         console.error("Có lỗi xảy ra khi gọi API:", error);
-//       }
-//     };
-//     getNewsData();
-//   }, []);
-//   console.log(homedata);
-
-//   return (
-//     // <div className=" content home-page">
-//     <Container className=" content home-page">
-//       <Row>
-//         <Col lg={9} md={12} className="content-left">
-//           <Row className="top_news-view">
-//             <Col lg={6} md={12} className="">
-//               <a href={`/newsdetail/${topViewedNews.id}`}>
-//                 <img
-//                   src={topViewedNews.anhdaidien}
-//                   alt={topViewedNews.tieude}
-//                 ></img>
-//               </a>
-//             </Col>
-//             <Col lg={6} md={12} className="">
-//               <div className="title">
-//                 <a
-//                   href={`/newsdetail/${topViewedNews.id}`}
-//                   className="main-title"
-//                 >
-//                   {topViewedNews.tieude}
-//                 </a>
-//                 <p className="summary-content">{topViewedNews.noidungtomtat}</p>
-//               </div>
-//             </Col>
-//           </Row>
-//           <Row className="news-by-category">
-//             {homedata.map(
-//               (category) =>
-//                 category.news.length > 0 && (
-//                   <div key={category.id} className="home">
-//                     <div className="category-and-tag d-flex align-items-center ">
-//                       <div className="title-category">
-//                         <a
-//                           href={`/categorys/${category.id}`}
-//                           className="category m-0"
-//                         >
-//                           {category.ten}
-//                         </a>
-//                       </div>
-//                       {/* <div className="tags">
-//                   {category.tags.slice(0,2).map((tag) => (
-//                     <span key={tag.id} className="tag">
-//                       {tag.ten}
-//                     </span>
-//                   ))}
-//                 </div> */}
-//                     </div>
-//                     <div className="news">
-//                       {category.news.slice(0, 4).map((newsItem, index) => (
-//                         <div key={newsItem.id}>
-//                           <div className="img">
-//                             <a href={`/newsdetail/${newsItem.id}`}>
-//                               <img
-//                                 src={newsItem.anhdaidien}
-//                                 alt={newsItem.tieude}
-//                               ></img>
-//                             </a>
-//                           </div>
-//                           <div className="title">
-//                             <a
-//                               href={`/newsdetail/${newsItem.id}`}
-//                               className="main-title"
-//                             >
-//                               {newsItem.tieude}
-//                             </a>
-
-//                             {index === 0 && (
-//                               <p className="summary-content m-0">
-//                                 {newsItem.noidungtomtat}
-//                               </p>
-//                             )}
-//                             {/* Chỉ hiển thị nội dung chi tiết cho tin tức đầu tiên */}
-//                           </div>
-//                         </div>
-//                       ))}
-//                     </div>
-//                   </div>
-//                 )
-//             )}
-//           </Row>
-//         </Col>
-//         <Col lg={3} md={12} className="content-right">
-//           <Row>
-//             <Col lg={12} md={6} className="mb-3">
-//               <LatestNewsList />
-//             </Col>
-//             <Col lg={12} md={6}>
-//               <RecommenNewsList setTopViewedNews={setTopViewedNews} />
-//             </Col>
-//           </Row>
-//         </Col>
-//       </Row>
-//       {loading && <p>loading...</p>}
-//     </Container>
-//     // </div>
-//   );
-// }
-
-// export default Home;
-
-
 import React, { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -165,7 +31,7 @@ function Home() {
     };
     getNewsData(page);
   }, [page]);
-  
+
   useEffect(() => {
     const handleScroll = () => {
       if (
@@ -202,7 +68,9 @@ function Home() {
                   >
                     {topViewedNews.tieude}
                   </a>
-                  <p className="summary-content">{topViewedNews.noidungtomtat}</p>
+                  <p className="summary-content">
+                    {topViewedNews.noidungtomtat}
+                  </p>
                 </div>
               </Col>
             </Row>
@@ -271,4 +139,3 @@ function Home() {
 }
 
 export default Home;
-
