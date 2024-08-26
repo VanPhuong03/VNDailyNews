982a79816364317d43c3b9777c0e604cb72f84a0
cdev <cuong170603@gmail.com>
2024-08-26 08:36:46 +0700

    refector: add copyright to footer of page

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -1,14 +1,22 @@
 import logo from "@assets/imgs/index";
 import "./index.scss";
 import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 function Footer() {
   return (
     <div className="footer">
       <div className="container">
-        <Col lg={4} className="p-5">
-          <img src={logo.logo} alt=""></img>
-        </Col>
+        <Row>
+          <Col lg={4}>
+            <div className="footer-logo">
+              <img src={logo.logo} alt=""></img>
+            </div>
+          </Col>
+          <Col lg={8} className="d-flex align-items-center justify-content-end">
+            <h6 className="footer-copyright mb-0">@2024 - Phát triển bời 👨‍💻cdev, linh chii và văn phương ✅</h6>
+          </Col>
+        </Row>
       </div>
     </div>
   );
